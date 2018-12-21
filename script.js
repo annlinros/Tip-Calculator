2b3100b0bf275595ae4e5e32726cfe4362b8c5aa
Anna Nidhin <42749886+annlinros@users.noreply.github.com>
2018-12-22 00:59:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculateTip(){
+    const totalBill = document.getElementById("bill").value;
+    const guests = document.getElementById("people").value;
+    const service = document.getElementById("service_type").value;
+    let tip;
+    if(totalBill == 0){
+        alert("Please enter the total bill!")
+    }
+    else{
+        if(guests > 1){
+            tip = (totalBill*service)/guests;
+            document.getElementById("tip").innerHTML = `Tip Amount: ${parseFloat(tip).toFixed(2)}$ each`;
+        }
+        else{
+            tip = totalBill*service;
+            document.getElementById("tip").innerHTML = `Tip Amount: ${parseFloat(tip).toFixed(2)}$`;
+        }
+    }
+}
+    
+document.getElementById("calculate").addEventListener("click", calculateTip);
+document.getElementById("service_type").addEventListener("change", calculateTip);
